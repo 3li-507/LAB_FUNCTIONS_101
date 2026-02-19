0875cc0ad270a4547db3b068c0680a73dd4145e5
Ali Alshehri <ps4ps5cc@gmail>
2026-02-19 12:38:50 +0300

    added help() for documentation

--- hierarchical.py
+++ hierarchical.py
@@ -1,19 +1,45 @@
 #Lab
 def hierarchical_num(n:int)->int:
+
+    '''
+    generate a hierarchical number as int
+    this function create a hierarchical num like this below:
+    if n=5 the output will be:
+    5 4 3 2 1
+    4 3 2 1
+    3 2 1
+    2 1
+    1
+    
+    '''
     for i in range(n,0,-1):
         for j in range(i,0,-1):
             print(j, end=" ")
         
-        print("")
+        print()
 
 
 hierarchical_num(5)
-print()
+
+help(hierarchical_num)
 
 
 
 #bouns
-def hierarchical_num(n:int)->str:
+def hierarchical_num2(n:int)->str:
+    '''
+    generate a hierarchical number as string
+    this function create a hierarchical num like this below:
+    if n=5 the output will be:
+    5 4 3 2 1
+    4 3 2 1
+    3 2 1
+    2 1
+    1
+
+
+    '''
+
 
     result=""
 
@@ -28,6 +54,9 @@ def hierarchical_num(n:int)->str:
 
 
 
-x=hierarchical_num(5)
-print(type(x))
-print(x)
+#x=hierarchical_num2(5)
+
+#print(x)
+
+
+help(hierarchical_num2)
